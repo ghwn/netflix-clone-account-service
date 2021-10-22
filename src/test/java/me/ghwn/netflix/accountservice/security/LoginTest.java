61ae4200ff4a28b52491e3032e427e0b9ecc0a2b
Jihwan Lim <ghwn5936@gmail.com>
2021-10-23 00:31:24 +0900

    test: Add test to issue new jwt token.

---
+++ src/test/java/me/ghwn/netflix/accountservice/security/LoginTest.java
@@ -0,0 +1,63 @@
+package me.ghwn.netflix.accountservice.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import me.ghwn.netflix.accountservice.dto.AccountCreationRequest;
+import me.ghwn.netflix.accountservice.dto.AccountLoginRequest;
+import me.ghwn.netflix.accountservice.entity.AccountRole;
+import me.ghwn.netflix.accountservice.service.AccountService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Set;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+public class LoginTest {
+
+    @Autowired WebApplicationContext webApplicationContext;
+    @Autowired ObjectMapper objectMapper;
+    @Autowired AccountService accountService;
+    @Autowired ModelMapper modelMapper;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(springSecurity())
+                .alwaysDo(print())
+                .build();
+    }
+
+    @Test
+    @DisplayName("Issue new JWT token")
+    void issueJwtToken() throws Exception {
+        // Sign up
+        AccountCreationRequest signupRequest = new AccountCreationRequest(
+                "user@example.com", "P@ssw0rd1234", true, Set.of(AccountRole.USER.name()));
+        accountService.createAccount(signupRequest);
+
+        // Login
+        AccountLoginRequest loginRequest = modelMapper.map(signupRequest, AccountLoginRequest.class);
+        mockMvc.perform(post("/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(loginRequest))
+                        .characterEncoding(StandardCharsets.UTF_8.name()))
+                .andExpect(status().isOk())
+                .andExpect(header().exists("X-Auth-Token"));
+    }
+}
