bc722af6af8b5b7cfd6704106e4f24db985f8a82
Jihwan Lim <ghwn5936@gmail.com>
2021-10-27 20:26:22 +0900

    test: Disable spring security when testing

--- src/test/java/me/ghwn/netflix/accountservice/controller/BaseControllerTest.java
+++ src/test/java/me/ghwn/netflix/accountservice/controller/BaseControllerTest.java
@@ -9,7 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,13 +17,11 @@ import org.springframework.web.context.WebApplicationContext;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @ExtendWith({RestDocumentationExtension.class})
 @SpringBootTest
 @Transactional
-@WithMockUser(username = "user@example.com", password = "P@ssw0rd1234", authorities = {"USER"})
 public abstract class BaseControllerTest {
 
     @Autowired WebApplicationContext webApplicationContext;
@@ -44,7 +41,6 @@ public abstract class BaseControllerTest {
 
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
                 .apply(documentationConfiguration(restDocumentation))
-                .apply(springSecurity())
                 .alwaysDo(print())
                 .alwaysDo(documentHandler)
                 .build();
